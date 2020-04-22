902980c5dda58b44a3e11fc651b38594eaacd320
sabbirhossain540 <m.sabbirhossain308@gmail.com>
2020-04-22 19:42:23 +0600

    Day 12

--- dental/settings.py
+++ dental/settings.py
@@ -123,5 +123,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'SMODULE-DENTAL-SERVICES')
+    os.path.join(BASE_DIR, 'dental/static')
 ]
