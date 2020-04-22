890e63241451eef099fd54b4e2c19d235be20514
sabbirhossain540 <m.sabbirhossain308@gmail.com>
2020-04-22 18:56:52 +0600

    Day 12

---
+++ virtual/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\md sabbir hossain\git\smodule-dental-services\virtual\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
