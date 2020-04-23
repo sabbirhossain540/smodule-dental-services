cace14be3ebf1adca647fdd05779b366de2cbcac
sabbirhossain540 <m.sabbirhossain308@gmail.com>
2020-04-23 17:51:16 +0600

    Day 13

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.mail import send_mail
 # Create your views here.
 
 def index(request):
@@ -13,7 +14,24 @@ def contact(request):
         sender_name = request.POST['sender_name']
         sender_email = request.POST['sender_email']
         message = request.POST['message']
-        print(message)
+        
+        #Send Email
+        send_mail(
+            #Subject
+            'Messege From SModule Dental Services '+sender_name
+            ,
+            #Messege
+            message
+            ,
+            #From Email
+            sender_email
+            ,
+            ['job.sabbirhossain308@gmail.com'],   #To Email
+
+            fail_silently=False 
+
+        )
+
         return render(request, 'pages/contact.html')
 
     else:
